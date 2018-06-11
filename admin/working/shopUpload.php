9d6b6c7e95dd7237377b3de9229e4848cc75ccdd
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-06-11 12:05:41 +0300

    Upload to DB
    shopUpload halfway to working DB for shop

--- admin/working/shopUpload.php
+++ admin/working/shopUpload.php
@@ -1,12 +1,9 @@
 <?php
 	include("config.php");
-	$dbname = "if17_Tantsumeka"; /* Database name */
-	$con = mysqli_connect($serverHost, $serverUsername, $serverPassword,$dbname);
-	// Check connection
-	if (!$con) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-
+	$database = "if17_Tantsumeka"
+	// Create connection
+	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+	
 	if(isset($_POST['but_upload'])){
 	
 		$name = $_FILES['file']['name'];
@@ -21,15 +18,18 @@
 
 		// Check extension
 		if( in_array($imageFileType,$extensions_arr) ){
-			// Convert to base64 
-			$image_base64 = base64_encode(file_get_contents($_FILES['file']['tmp_name']) );
-			$image = 'data:image/'.$imageFileType.';base64,'.$image_base64;
-			// Insert record
-			$query = "insert into products(image) values('".$image."')";
-			mysqli_query($con,$query);
-			
-			// Upload file
-			move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
+			// save to database
+			if(isset($_POST["name"]) and isset($_POST["description"]) and isset($_POST["price"]) and isset($_POST["category"]) and isset($_POST["heel"]) ){
+				$stmt = $mysqli->prepare("INSERT INTO products (name, image, description, price, category, heel) VALUES (?, .$target_dir., ?, ?, ?,?)");
+				echo $mysqli->error;
+				$stmt->bind_param("ssiss", $_POST["name"],$_POST["description"],$_POST["price"], $_POST["category"],$_POST["heel"]);
+				$stmt->execute();
+				echo $stmt->error;
+				// Upload file
+				move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
+			}
+			$stmt->close();
+			$mysqli->close();
 		}	
 	}
 ?>
@@ -44,6 +44,11 @@
 	<section>
 		<div>
             <h1>Uploader</h1>
+			<input placeholder="Nimetus" name="name" type="text" required="">
+			<input placeholder="kirjeldus" name="description" type="text" required="">
+			<input placeholder="hind" name="price" type="text" required="">
+			<input placeholder="kategooria" name="category" type="text" required="">
+			<input placeholder="konks" name="heel" type="text" required="">
 			<form method="post" action="" enctype='multipart/form-data'>
  				<input type='file' name='file' />
   				<input type='submit' value='Save name' name='but_upload'>
