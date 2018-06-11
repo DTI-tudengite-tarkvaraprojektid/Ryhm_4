bf2863afa9395bb71d400e2b50cc8314c396bd8c
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-06-11 09:58:14 +0300

    Shop
    added shopUpload page

---
+++ admin/working/shop/shopUpload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Tantsumeka | admin</title>
+</head>
+<body>
+	<section class="container-fluid">
+		<div>
+            <h1>Uploader</h1>
+        </div>
+	</section>
+</body>
+</html>
