053ba439301045546087f7ac088a55bc6e8743c1
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-06-11 13:11:01 +0300

    ShopUpload
    SQL REPAIRED

--- admin/working/shopUpload.php
+++ admin/working/shopUpload.php
@@ -12,32 +12,33 @@
 	if(isset($_POST["but_upload"])){
 
 		if(isset($_POST["description"]) and isset($_POST["price"]) and isset($_POST["category"]) and isset($_POST["heel"]) ){
+			// Photo upload 
+			$name = $_FILES['file']['name'];
+			$target_dir = "shop/upload/";
+			$target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+			// Select file type
+			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+			// Valid file extensions
+			$extensions_arr = array("jpg","jpeg","png","gif");
+
+			// Check extension
+			if( in_array($imageFileType,$extensions_arr) ){			
+				// Upload file
+				move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
+			}	
 		
-			$stmt = $mysqli->prepare("INSERT INTO  products (description, price, category, heel) VALUES (?, ?, ?, ?)");
+			$stmt = $mysqli->prepare("INSERT INTO  products (name, description, price, category, heel, img) VALUES (?,?,?,?,?,'$name')");
 			echo $mysqli->error;
-			$stmt->bind_param("ssss", $_POST["description"], $_POST["price"],$_POST["category"],$_POST["heel"]);
+			$stmt->bind_param("sssss", $_POST["name"],$_POST["description"], $_POST["price"],$_POST["category"],$_POST["heel"]);
 			$stmt->execute();
 			echo $stmt->error;
 			
 		}
 		$stmt->close();
 		$mysqli->close();
-		// Photo upload 
-		$name = $_FILES['file']['name'];
-		$target_dir = "shop/upload/";
-		$target_file = $target_dir . basename($_FILES["file"]["name"]);
-
-		// Select file type
-		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-		// Valid file extensions
-		$extensions_arr = array("jpg","jpeg","png","gif");
-
-		// Check extension
-		if( in_array($imageFileType,$extensions_arr) ){			
-			// Upload file
-			move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
-		}	
+		
 	}
 ?>
 
@@ -52,12 +53,13 @@
 		<div>
             <h1>Uploader</h1>
 			<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype='multipart/form-data'>
+				<input placeholder="nimetus" name="name" type="text" required="">
 				<input placeholder="kirjeldus" name="description" type="text" required="">
 				<input placeholder="hind" name="price" type="text" required="">
 				<input placeholder="kategooria" name="category" type="text" required="">
 				<input placeholder="konks" name="heel" type="text" required="">
  				<input type='file' name='file' />
-  				<input type='submit' value='Save name' name='but_upload'>
+  				<input type='submit' value='Save' name='but_upload'>
 			</form>
         </div>
 	</section>
