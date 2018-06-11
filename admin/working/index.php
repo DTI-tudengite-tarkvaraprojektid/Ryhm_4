25992ac1713e00e617b3f78487e4c1fe63ee9f6d
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-06-11 14:34:57 +0300

    Admin Index
    shopUpload imported to admin index

--- admin/working/index.php
+++ admin/working/index.php
@@ -10,28 +10,40 @@ $database = "if17_Tantsumeka";
 		header("Location: login.php");
 	}
 
-// Create connection
-$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
-	$usermail = $_SESSION["userEmail"];
-	//echo $usermail;
-	if(isset($_POST["submit"])){
+    // Create connection
+    $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 
-		if(isset($_POST["name"]) and isset($_POST["number"]) and isset($_POST["quantity"])){
-			//echo $_POST["name"];
+	if(isset($_POST["but_upload"])){
 
-			$stmt = $mysqli->prepare("INSERT INTO Retsept (name, number, quantity, email) VALUES (?, ?, ?, '$usermail')");
+		if(isset($_POST["description"]) and isset($_POST["price"]) and isset($_POST["category"]) and isset($_POST["heel"]) ){
+			// Photo upload 
+			$name = $_FILES['file']['name'];
+			$target_dir = "shop/upload/";
+			$target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+			// Select file type
+			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+			// Valid file extensions
+			$extensions_arr = array("jpg","jpeg","png","gif");
+
+			// Check extension
+			if( in_array($imageFileType,$extensions_arr) ){			
+				// Upload file
+				move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
+			}	
+		
+			$stmt = $mysqli->prepare("INSERT INTO  products (name, description, price, category, heel, image) VALUES (?,?,?,?,?,'$name')");
 			echo $mysqli->error;
-			$stmt->bind_param("sii", $_POST["name"], $_POST["number"],$_POST["quantity"]);
+			$stmt->bind_param("sssss", $_POST["name"],$_POST["description"], $_POST["price"],$_POST["category"],$_POST["heel"]);
 			$stmt->execute();
 			echo $stmt->error;
 			
 		}
-	$stmt->close();
-	$mysqli->close();
+		$stmt->close();
+		$mysqli->close();
+		
 	}
-	//echo $usermail;
-	
-
 
 	//kui pole sisseloginud, siis sisselogimise lehele
 	if(!isset($_SESSION["userId"])){
@@ -55,19 +67,32 @@ $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
     </head>
     <body>
         <div class="sidenav">
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
+            <a href="#">Pood</a>
+            <a href="#">Services</a>
             <a href="#clients">Clients</a>
             <a href="#contact">Contact</a>
         </div>
-        <div class="main">
-            <h2>Sidebar</h2>
-            <p>This sidebar is of full height (100%) and always shown.</p>
-            <p>Scroll down the page to see the result.</p>
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+        <div class="main" id="shop">
+            <div id="upload"> 
+                <h1>Uploader</h1>
+                <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype='multipart/form-data'>
+                    <input placeholder="nimetus" name="name" type="text" required="">
+                    <input placeholder="kirjeldus" name="description" type="text" required="">
+                    <input placeholder="hind" name="price" type="text" required="">
+                    <input placeholder="kategooria" name="category" type="text" required="">
+                    <select name="heel">
+                        <option value="5cm peenkonts">5cm peenkonts</option>
+                        <option value="7cm peenkonts">7cm peenkonts</option>
+                        <option value="8cm peenkonts">8cm peenkonts</option>
+                        <option value="5cm alt laienev konst">5cm alt laienev konst</option>
+                        <option value="7cm alt laienev konst">7cm alt laienev konst</option>
+                        <option value="8cm alt laienev konst">8cm alt laienev konst</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                    <input type='file' name='file' />
+                    <input type='submit' value='Save' name='but_upload'>
+                </form>
+            </div>
         </div>
         
     </body>
