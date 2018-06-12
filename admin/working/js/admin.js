08961d419fdca36c51a90388d6271825a0baa6d9
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-06-12 09:17:51 +0300

    ADMIN INDEX
    DELETED STYLE

--- admin/working/js/admin.js
+++ admin/working/js/admin.js
@@ -1,35 +1,3 @@
-//left side menu based on  https://codepen.io/j_holtslander/pen/XmpMEp
-$(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-       isClosed = false;
-  
-      trigger.click(function () {
-        hamburger_cross();      
-      });
-  
-      function hamburger_cross() {
-  
-        if (isClosed == true) {          
-          overlay.hide();
-          trigger.removeClass('is-open');
-          trigger.addClass('is-closed');
-          isClosed = false;
-        } else {   
-          overlay.show();
-          trigger.removeClass('is-closed');
-          trigger.addClass('is-open');
-          isClosed = true;
-        }
-    }
-    
-    $('[data-toggle="offcanvas"]').click(function () {
-          $('#wrapper').toggleClass('toggled');
-    });  
-  });
-
-
-
 //GLOBAL VARIABLES
 var shop = document.getElementById("shop");
 var calendar = document.getElementById("calendar");
