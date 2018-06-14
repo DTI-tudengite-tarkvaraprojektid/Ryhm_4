12e2c66048b5b54ba57c3279edd0a196e7a99927
Konstnatin Blinkov <31948927+kostjaki@users.noreply.github.com>
2018-06-14 10:56:37 +0300

    Toode tellimus lehe mittu pilte

--- Shop/views/pages/cat.php
+++ Shop/views/pages/cat.php
@@ -8,10 +8,10 @@ foreach($products as $item){?>
 <table align="center" cellpadding="1" cellspacing="1" class="product" border="1">
 	<tr>
 		<td valign="top">
-			<div><a href="?view=product&id=<?php echo $item['id']?>"><img src="images/<?php echo $item['image']?>" alt="" style="width:100px;/></a></div>
+			<div><a href="?view=product&id=<?php echo $item['id']?>"><img src="images/<?php echo $item['image_chosen']?>" alt="" style="width:100px;/></a></div>
 			<div class="description">
 				<div class="product-name"><a href="#"><?php echo $item['name']?></a></div>
-				<div class="product-price">Price: <?php echo $item['price']?>$</div>
+				<div class="product-price">Price: <?php echo $item['price']?>€</div>
 			</div>
 		</td>
 	</tr>
--- Shop/views/pages/index.php
+++ Shop/views/pages/index.php
@@ -16,10 +16,10 @@ foreach($products as $item){?>
 			<table align="center" cellpadding="1" cellspacing="1" class="product" border="0">
 				<tr>
 					<td valign="top">
-						<div><a href="index.php?view=product&id=<?php echo $item['id']?>"><img class="card-img-top" src="images/<?php echo $item['image']?>" alt="" /></a></div>
+						<div><a href="index.php?view=product&id=<?php echo $item['id']?>"><img class="card-img-top" src="images/<?php echo $item['image_chosen']?>" alt="" /></a></div>
 						<div class="description">
 							<div class="product-name"><a href="#"><?php echo $item['name']?></a></div>
-							<div class="product-price">Price: <?php echo $item['price']?>$</div>
+							<div class="product-price">Price: <?php echo $item['price']?>€</div>
 						</div>
 					</td>
 				</tr>
