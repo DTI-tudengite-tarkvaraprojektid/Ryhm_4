f6b771707bc32c97912832b4bd24786b80fc22b9
Tauri Taevik <31793402+ttaevik@users.noreply.github.com>
2018-06-16 17:05:10 +0300

    shop product update

--- Shop/views/pages/product.php
+++ Shop/views/pages/product.php
@@ -7,6 +7,15 @@
 	$heel_count = count($heel);
 ?>
 <form action="https://formspree.io/robert.shev.spam@gmail.com" class="form-horizontal" method="POST">
+	<script>
+					function getValuesKonst() {
+						document.getElementById("konst").value = document.getElementById("konstVal").value;
+					}
+					function getValuesSize(){
+						document.getElementById("size").value = document.getElementById("sizeVal").value;
+					}
+	</script>
+
 	<div class="container">
 		<div class="display-4 text-center">
 			<div class="">
@@ -16,15 +25,21 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div>
-					<img id='i1' src="images/<?php echo $pics[0];?>" height="300"/><br />
+					<img id='i1' src="images/<?php echo $pics[0];?>"class="img-fluid large" /><br />
 				</div>
 				<?php 
 				for ($i=0;$i<$pics_count;$i++)
 				{ ?>
-					<a href="#" onclick="document.getElementById('i1').src='images/<?php echo $pics[$i];?>'"><img src="images/<?php echo $pics[$i];?>" class = "image-fluid small" height="80"/></a>
+					<a href="#" onclick="document.getElementById('i1').src='images/<?php echo $pics[$i];?>'"><img src="images/<?php echo $pics[$i];?>" class = "img-fluid small" height="80"/></a>
 				<?php } ?>
 			</div>
 			<div class="col-md-3">
+				
+				<a>Teie andmed:</a>
+				<input class="form-control" type="text" placeholder="Nimi" name="name" required>
+				<input class="form-control" type="email" placeholder="Email" name="_replyto" required>
+				<input class="form-control" type="text" placeholder="Telefoninumber" name="Tel" required><br>
+
 				<input type="hidden" name="toode" value="<?php $product['name']?>" />
 				<a>Vali kinga suurus</a>
 				<select class="form-control" id="sizeVal" onclick="getValuesSize()">
@@ -63,38 +78,38 @@
 				</select> 
 				<input id="konst" type="hidden" name="Konst" value="" />
 				<a href="#">Vaata kontsa infot siit</a><br><br>
-				<a>Juhul kui teil on mittestandartne jalg, palume täita teie jala mõõdud</a><br>
+				<a data-toggle="collapse"  onclick="collapsePanels()" href="#mittestandard" aria-expanded="false" aria-controls="mittestandard">Kui teil on mittestandartne jalg, siis vajutage siia</a>
+				<div class="collapse" id="mittestandard">
 				<input class="form-control" type="text" placeholder="Vasaku jala mõõt 1 (mm):" name="Vasaku jala mõõt 1 (mm)"/>
 				<input class="form-control" type="text" placeholder="Vasaku jala mõõt 2 (mm):" name="Vasaku jala mõõt 2 (mm)"/>
 				<input class="form-control" type="text" placeholder="Vasaku jala mõõt 3 (mm):" name="Vasaku jala mõõt 3 (mm)"/>
 				<input class="form-control" type="text" placeholder="Parema jala mõõt 1 (mm):" name="Parema jala mõõt 1 (mm)"/>
 				<input class="form-control" type="text" placeholder="Parema jala mõõt 2 (mm):" name="Parema jala mõõt 2 (mm)"/>
 				<input class="form-control" type="text" placeholder="Parema jala mõõt 3 (mm):" name="Parema jala mõõt 3 (mm)"/>
-				<a href="#">Mõõtmisõpetust vaata siitt</a><br><br>
-				<script>
-					function getValuesKonst() {
-						document.getElementById("konst").value = document.getElementById("konstVal").value;
-					}
-					function getValuesSize(){
-						document.getElementById("size").value = document.getElementById("sizeVal").value;
-					}
-				</script>
-			</div>
-			<div class="col-md-6">
-				<a>Teie andmed:</a><br>
-					<input class="form-control" type="text" placeholder="Nimi" name="name" required>
-					<input class="form-control" type="email" placeholder="Email" name="_replyto" required>
-					<input class="form-control" type="text" placeholder="Telefoninumber" name="Tel" required><br>
-			</div>
-			<div class="col-md-3">
+				<a href="#">Mõõtmisõpetust vaata siit</a>
+				</div>
+				<div class="row">
+				<div class="col-md-4">
 				<a>Kogus:</a><input class="form-control" type="text" value="1" placeholder="Kogus" name="kogus"/>
+				</div>
+				<div class="col-md-4">
 				<a>Hind: </a><a style="color:red"><?php echo $product['price']?></a><input type="hidden" name="hind" value="<?php echo $product['price']?>" /><br>
+				</div>
+				<div class="col-md-4">
 				<input type="hidden" name="_next" value="http://www.tlu.ee/~shev/eesrakenduse/sayings-offline-app/" />
 				<input type="hidden" name="_subject" value="Tellimine" />
 				<input type="hidden" name="_format" value="plain" />
 				<input type="text" name="_gotcha" style="display:none" />
 				<input class="btn btn-primary1" type="submit" value="Telli"/>
+				</div>
+				</div>
 			</div>
+			
+			
+				
+				
+			
+			
 		</div>
 	</div>
 </form>
