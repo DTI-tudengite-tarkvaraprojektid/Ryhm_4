fa93e012ffef70dec19317a87130e1a697969635
Tauri Taevik <31793402+ttaevik@users.noreply.github.com>
2018-06-19 12:30:57 +0300

    link finx

--- main/header.php
+++ main/header.php
@@ -11,46 +11,46 @@
 			<li class="nav-item dropdown">
 			    <a class="nav-link" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#" style="color: #ffef8f;">KURSUSED</a>
 			    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			     <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/kursused/algkursus/" target="_self">Algkursus</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/kursused/jatkukursus/" target="_self">Jätkukursus</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/kursused/edasijoudnud/" target="_self">Edasijõudnute kursus</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/kursused/kalender/" target="_self">Kalender</a>
+			     <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/kursused/algkursus/" target="_self">Algkursus</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/kursused/jatkukursus/" target="_self">Jätkukursus</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/kursused/edasijoudnud/" target="_self">Edasijõudnute kursus</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/kursused/kalender/" target="_self">Kalender</a>
 			    </div>
 			</li>
 			<li class="nav-item dropdown">
 			    <a class="nav-link"  data-toggle="dropdown" href="#" style="color: #ffef8f;">HOBITANTS</a>
 			    <div class="dropdown-menu">
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/hobitants/hobiklubi/" target="_self">Hobiklubi</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/hobitants/kalender/" target="_self">Kalender</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/hobitants/hobiklubi/" target="_self">Hobiklubi</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/hobitants/kalender/" target="_self">Kalender</a>
 			    </div>
 			</li>
 			<li class="nav-item dropdown">
 			    <a class="nav-link" data-toggle="dropdown" href="#" style="color: #ffef8f;">VÕISTLUSTANTS</a>
 			    <div class="dropdown-menu">
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/voistlustants/taiskasvanutele/" target="_self">Võistlusttants</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//voistlustants/kalender/" target="_self">Kalender</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/voistlustants/taiskasvanutele/" target="_self">Võistlusttants</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/voistlustants/kalender/" target="_self">Kalender</a>
 			    </div>
 			</li>
 			<li class="nav-item dropdown">
 			    <a class="nav-link" data-toggle="dropdown" href="#" style="color: #ffef8f;">ERATUNNID</a>
 			    <div class="dropdown-menu">
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/eratunnid/pulmatants/" target="_self">Pulmatants</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/eratunnid/seltskonnatants/" target="_self">Seltskonnatants</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/eratunnid/voistlustants/" target="_self">Võistlustants</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/eratunnid/kalender/" target="_self">Kalender</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/eratunnid/pulmatants/" target="_self">Pulmatants</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/eratunnid/seltskonnatants/" target="_self">Seltskonnatants</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/eratunnid/voistlustants/" target="_self">Võistlustants</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/eratunnid/kalender/" target="_self">Kalender</a>
 			    </div>
 			</li>
 			<li class="nav-item dropdown">
 			    <a class="nav-link" data-toggle="dropdown" href="#" style="color: #ffef8f;">TANTSUTARBED</a>
 			    <div class="dropdown-menu">
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//Shop/" target="_self">Tantsukingad</a>
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4//main/shop/tantsuroivad/" target="_self">Tantsurõivad</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/Shop/" target="_self">Tantsukingad</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/shop/tantsuroivad/" target="_self">Tantsurõivad</a>
 			    </div>
 			</li>
 			<li class="nav-item dropdown">
 			    <a class="nav-link" data-toggle="dropdown" href="#" style="color: #ffef8f;">LISAKS</a>
 			    <div class="dropdown-menu">
-			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/lisaks/galerii" target="_self">Galerii</a>
+			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/lisaks/galerii" target="_self">Galerii</a>
 			      <a class="dropdown-item" href="http://greeny.cs.tlu.ee/~sevtrobe/Tantsumeka/Ryhm_4/main/lisaks/kontakt/" target="_self">Kontakt</a>
 			    </div>
 			</li>
