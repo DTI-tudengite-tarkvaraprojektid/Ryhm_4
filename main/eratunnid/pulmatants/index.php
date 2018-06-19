484d7aa694384e5ed188e0276079956a2c3e6d3d
Tauri Taevik <31793402+ttaevik@users.noreply.github.com>
2018-06-19 13:33:32 +0300

    added google calendar, favicon fix

--- main/eratunnid/pulmatants/index.php
+++ main/eratunnid/pulmatants/index.php
@@ -10,23 +10,15 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 	<link href="https://greeny.cs.tlu.ee/~valevale/praktika/Ryhm_4/main/assets/css/style.css" rel="stylesheet">
-	<link rel="apple-touch-icon" sizes="57x57" href="../../favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="../../favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="../../favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="../../favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="../../favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="../../favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="../../favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="../../favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="../../favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="../../favicon/android-icon-192x192.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../../favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="../../favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="../../favicon/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="../../favicon/favicon-16x16.png">
-	<link rel="manifest" href="../../favicon/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="../../favicon/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
+	<link rel="manifest" href="../../favicon/site.webmanifest">
+	<link rel="mask-icon" href="../../favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="../../favicon/favicon.ico">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-config" content="../../favicon/browserconfig.xml">
+	<meta name="theme-color" content="#ffef8f">
 	<script>
 		function collapsePanels(){
 			jQuery('.collapse').collapse('hide');
@@ -66,10 +58,10 @@
 						<textarea class="form-control" rows="14" name="sõnum" type="text" placeholder="Pärast registreerimist võtame Teiega ühendust." required></textarea>
 						<div class="invalid-feedback">Palun täitke väli.</div>
 					</div>
-					<input name="_next" value="http://www.tlu.ee/~shev/eesrakenduse/sayings-offline-app/" type="hidden">
-					<input name="_subject" value="Registreerimine" type="hidden">
-					<input name="_format" value="plain" type="hidden">
-					<input name="_gotcha" style="display:none" type="text">
+						<input name="_next" value="http://www.tlu.ee/~shev/eesrakenduse/sayings-offline-app/" type="hidden">
+						<input name="_subject" value="Registreerimine" type="hidden">
+						<input name="_format" value="plain" type="hidden">
+						<input name="_gotcha" style="display:none" type="text">
 					<div class="form-group">
 						<input class="btn btn-primary" value="Registreeri" type="submit">
 					</div>
--- main/lisaks/kontakt/index.php
+++ main/lisaks/kontakt/index.php
@@ -10,23 +10,15 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 	<link href="https://greeny.cs.tlu.ee/~ttaevik/test/Ryhm_4/main/assets/css/style.css" rel="stylesheet">
-	<link rel="apple-touch-icon" sizes="57x57" href="../../favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="../../favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="../../favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="../../favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="../../favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="../../favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="../../favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="../../favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="../../favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="../../favicon/android-icon-192x192.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../../favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="../../favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="../../favicon/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="../../favicon/favicon-16x16.png">
-	<link rel="manifest" href="../../favicon/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="../../favicon/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
+	<link rel="manifest" href="../../favicon/site.webmanifest">
+	<link rel="mask-icon" href="../../favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="../../favicon/favicon.ico">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-config" content="../../favicon/browserconfig.xml">
+	<meta name="theme-color" content="#ffef8f">
 	<script>
 		function hideHeaderText() {
     		var x = document.getElementById("tutvustus");
@@ -56,11 +48,13 @@
 	<div class="col-lg-6" id="tantsud">
 		<div class="card card-body" id="tabp">
 			<h3 class="text-center">Rocca Al Mare Kool, Vabaõhumuuseumi tee 8, 13521 Tallinn; </h3>
+			<div class="googleCalendar"><iframe  id="gmap_canvas" src="https://maps.google.com/maps?q=vaba%C3%B5humuuseumi%20tee%208&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
 		</div>
 	</div>
 	<div class="col-lg-6" id="tantsud">
 		<div class="card card-body" id="tabp">
 			<h3 class="text-center">Haabersti Vene Gümnaasium, Õismäe tee 132, 13511 Tallinn; </h3>
+			<div class="googleCalendar"><iframe  id="gmap_canvas" src="https://maps.google.com/maps?q=%C3%B5ism%C3%A4e%20tee%20132&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
 		</div>
 	</div>
 	<div class="col-lg-12" id="tantsud">
